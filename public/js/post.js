c7d0a8752aec09ae073644b5376124e974e75aa6
Frank Nguyen <frank@ultra.me>
2023-01-23 16:31:13 -0800

    add post feature

---
+++ public/js/post.js
@@ -0,0 +1,31 @@
+
+const postFormHandler = async (event) => {
+    event.preventDefault();
+    
+    const name = document.querySelector('#post-name').value;
+    const description = document.querySelector('#post-description').value
+    try {
+        const response = await fetch('/api/post', {
+        method: 'POST',
+        body: JSON.stringify({ 
+					name, 
+					description, 
+        }),
+        headers: { 'Content-Type': 'application/json' },
+    });
+    if (response.ok) {
+        window.location.replace('/dashboard');
+    } else {
+        alert('Failed to create post');
+        }
+    }
+    catch(err) {
+        res.status(400).json(err);
+    }
+};
+
+        
+document
+  .querySelector('.post-form')
+  .addEventListener('submit', postFormHandler);
+
