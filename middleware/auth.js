84668b87e6445ecd428ee947d00dda34447a2738
Frank Nguyen <frank@ultra.me>
2023-01-24 15:10:30 -0800

    added middleware auth

---
+++ middleware/auth.js
@@ -0,0 +1,10 @@
+const isLoggedIn = (req, res, next) => {
+    if (!req.session.logged_in) {
+      res.redirect('/login');
+    } else {
+      next();
+    }
+  };
+  
+  module.exports = isLoggedIn;
+  
