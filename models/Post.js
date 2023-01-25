2fa5a4193bd6538f00c2dce2541e54df6ab72abc
Frank Nguyen <frank@ultra.me>
2023-01-25 13:53:14 -0800

    fixed post model to take in TEXT and not string

--- models/Post.js
+++ models/Post.js
@@ -17,7 +17,7 @@ Post.init(
         allowNull: false,
       },
       description: {
-        type: DataTypes.STRING,
+        type: DataTypes.TEXT,
       },
       date_created: {
         type: DataTypes.DATE,
