bc5de8c09592d1021a9e8711f571d1c6cfa78cd3
Frank Nguyen <frank@ultra.me>
2023-01-25 13:33:12 -0800

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 let sequelize;
 
-if (procewss.env.JAWSDB_URL) {
+if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
